996e82d54ec56966bcc05cd9b38be0c9fed1682d
Joshua Aresty <jaresty@gmail.com>
2024-06-16 22:19:28 -0700

    Removes stray print

--- GPT/gpt.py
+++ GPT/gpt.py
@@ -40,7 +40,6 @@ def gpt_query(prompt: str, content: str) -> str:
     url = settings.get("user.model_endpoint")
 
     headers, data = generate_payload(prompt, content)
-    print(data)
     response = requests.post(url, headers=headers, data=json.dumps(data))
 
     match response.status_code:
