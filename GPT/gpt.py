86ab162d0d91c2b1f269343a1da1dc36fc5622cf
Schwa Aresty <jaresty@gmail.com>
2024-07-14 21:38:27 -0700

    model chain will fail unless last was pasted is true (#95)

--- GPT/gpt.py
+++ GPT/gpt.py
@@ -209,13 +209,13 @@ class UserActions:
             case "above":
                 actions.key("left")
                 actions.edit.line_insert_up()
-                paste_and_modify(result, modifier)
                 GPTState.last_was_pasted = True
+                paste_and_modify(result, modifier)
             case "below":
                 actions.key("right")
                 actions.edit.line_insert_down()
-                paste_and_modify(result, modifier)
                 GPTState.last_was_pasted = True
+                paste_and_modify(result, modifier)
             case "clipboard":
                 clip.set_text(result)
             case "appendClipboard":
@@ -237,8 +237,8 @@ class UserActions:
             case "cursorless":
                 actions.user.cursorless_insert(cursorless_destination, result)
             case "paste" | _:
-                paste_and_modify(result, modifier)
                 GPTState.last_was_pasted = True
+                paste_and_modify(result, modifier)
 
     def gpt_get_source_text(spoken_text: str) -> str:
         """Get the source text that is will have the prompt applied to it"""
