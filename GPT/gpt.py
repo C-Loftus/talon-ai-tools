0d90daa72071c475f93d079467aebdb2aef284cb
Joshua Aresty <jaresty@gmail.com>
2024-08-12 11:43:14 -0700

    Respond to pull request feedback

--- GPT/gpt.py
+++ GPT/gpt.py
@@ -137,17 +137,17 @@ class UserActions:
         """Apply an arbitrary prompt to arbitrary text"""
 
         text_to_process: GPTMessageItem = actions.user.gpt_get_source_text(source)
+        if (
+            text_to_process.get("text", "") == ""
+            and text_to_process.get("image_url", "") == ""
+        ):
+            text_to_process = None  # type: ignore
 
         # Handle special cases in the prompt
         ### Ask is a special case, where the text to process is the prompted question, not selected text
         if prompt.startswith("ask"):
             text_to_process = format_message(prompt.removeprefix("ask"))
             prompt = "Generate text that satisfies the question or request given in the input."
-        elif (
-            text_to_process.get("text", "") == ""
-            and text_to_process.get("image_url", "") == ""
-        ):
-            text_to_process = None  # type: ignore
 
         response = gpt_query(format_message(prompt), text_to_process, destination)
 
