1290cc46ff7c7b00b43cc7a8166c96a453093a0c
Max Foxley-Marrable <22856173+Max-FM@users.noreply.github.com>
2024-09-25 21:53:43 -0400

    Apply header fix to image generation

--- Images/ai-images.py
+++ Images/ai-images.py
@@ -15,10 +15,12 @@ class Actions:
 
         url = "https://api.openai.com/v1/images/generations"
         TOKEN = get_token()
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {TOKEN}",
-        }
+        headers = {"Content-Type": "application/json"}
+        # If the model endpoint is Azure, we need to use a different header
+        if "azure.com" in url:
+            headers["api-key"] = TOKEN
+        else:
+            headers["Authorization"] = f"Bearer {TOKEN}"
         data = {
             "model": "dall-e-3",
             "prompt": prompt,
